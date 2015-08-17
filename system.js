3bf27f60e8124fbfaaddd1bd938cc74ae36568f4
michael-obrien <michael_obrien_a@hotmail.com>
2015-08-17 22:45:19 +0000

    first commit

---
+++ system.js
@@ -0,0 +1,19 @@
+// Define the system topology here. The topolgy should reference 
+// containers defined in definitions/services.js
+
+exports.name = 'ZasBB_Get_Thread';
+exports.namespace = 'ZasBB';
+exports.id = 'fa6769c5-9989-4c7d-ac1c-a10d87aececa';
+
+exports.topology = {
+  development: {
+  }
+};
+
+// Example
+//
+// exports.topology = {
+//   development: {
+//     root: ['web']
+//   }
+// };
